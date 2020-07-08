85371cd884fc1db203b3d67ab5f8a16e37f27a0a
Shinjan Mitra <shinjanxp@gmail.com>
2020-07-08 21:29:25 +0530

    feat: Adds directory structure parsing from csv store

--- csv-store-fetch.py
+++ csv-store-fetch.py
@@ -1,6 +1,7 @@
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
 import csv, os, datetime
+import shutil
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,21 +10,29 @@ sourcePathPrefix=os.getenv('SOURCE_PREFIX', '/home/Data')
 sourcePathSuffix=os.getenv('SOURCE_SUFFIX', '/data')
 destPathPrefix=os.getenv('DEST_PREFIX', '/home/dest')
 
-starttime=datetime.datetime(2019,7,17,0,0).timestamp()
-increment = 24*3600
-endtime=datetime.datetime(2020,5,12,0,0).timestamp()
+starttime=datetime.datetime(2019,7,17,0,0)
+endtime=datetime.datetime(2020,5,12,0,0)
 sensorIdx=1
 sensorMap={}
-d=datetime.datetime.fromtimestamp(starttime)
+d=starttime
 
-path=os.path.join(sourcePathPrefix, d.strftime("%Y/%m/%d"), sourcePathSuffix)
-for filename in os.listdir(path):
-    sensorId = filename.split("_")[0]
-    if sensorId in sensorMap:
+while d<= endtime:
+    print(d)
+    path=os.path.join(sourcePathPrefix, d.strftime("%Y/%m/%d"), sourcePathSuffix)
+    for filename in sorted(os.listdir(path)):
+        sensorId = filename.split("_")[0]
+        if sensorId not in sensorMap:
+            sensorMap[sensorId] = 'R%s'%sensorIdx
+            sensorIdx+=1
         idx = sensorMap[sensorId]
-    else:
-        sensorMap[sensorId] = sensorIdx
-        sensorIdx+=1
-    print(id)
-print(path)
+        # print(sensorId, idx)
+
+        destDirectory = "data/%s"%(idx)
+
+        if not os.path.exists(destDirectory):
+            os.mkdir(destDirectory)
+
+        shutil.copyfile(os.path.join(path,filename), os.path.join(destDirectory, filename.split("_")[1]))
+    # print(path)
+    d += datetime.timedelta(days=1)
 
