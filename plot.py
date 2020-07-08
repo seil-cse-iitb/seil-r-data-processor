7b481460d331c498801ee0c62d9e91e28e1ffdb9
Shinjan Mitra <shinjanxp@gmail.com>
2020-07-08 14:16:43 +0530

    feat: Adds heatmap plot script using analysis data

---
+++ plot.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('analysis.csv')
+dates = list(df['date'])
+sensors = list(df)[1:]
+availability = df
+del availability['date']
+availability = availability.transpose()
+print(availability)
+
+fig, ax = plt.subplots()
+im = ax.imshow(availability)
+
+# We want to show all ticks...
+# ax.set_xticks(np.arange(len(dates)))
+ax.set_yticks(np.arange(len(sensors)))
+# ... and label them with the respective list entries
+# ax.set_xticklabels(dates)
+ax.set_yticklabels(sensors)
+
+# Rotate the tick labels and set their alignment.
+# plt.setp(ax.get_xticklabels(), rotation=45, ha="right",rotation_mode="anchor")
+
+ax.set_title("Availability of sensor data in 2017")
+# fig.tight_layout()
+fig.set_size_inches(50, 10)
+
+plt.savefig("availability.png")
