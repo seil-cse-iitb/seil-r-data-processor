d909d023e8724d571706d2c0df8aae40d4d0485c
Shinjan Mitra <shinjanxp@gmail.com>
2020-07-27 14:18:10 +0530

    feat: Adds file existence checking

--- missing-analysis-csv-store.py
+++ missing-analysis-csv-store.py
@@ -1,6 +1,6 @@
 # import pandas as pd
 # import numpy as np
-import csv, os, datetime
+import csv, os, datetime, json
 import shutil
 
 from dotenv import load_dotenv
@@ -9,16 +9,18 @@ load_dotenv()
 sourcePathPrefix=os.getenv('SOURCE_PREFIX', '/home/Data')
 sourcePathSuffix=os.getenv('SOURCE_SUFFIX', '/data')
 destPathPrefix=os.getenv('DEST_PREFIX', '/home/dest')
+startDateStr = os.getenv('START_DATE', '2019-01-31')
+endDateStr = os.getenv('END_DATE', '2019-12-31')
 
-starttime=datetime.datetime(2018,1,1,0,0)
-endtime=datetime.datetime(2019,12,31,0,0)
+startDate=datetime.strptime(startDateStr, '%Y-%m-%d')
+endDate=datetime.strptime(endDateStr, '%Y-%m-%d')
 sensorIdx=1
 sensorMap={}
-d=starttime
+d=startDate
 
 # First we need to get a list of all the sensors
 # Iterate through the list of dates
-while d<= endtime:
+while d<= endDate:
     print(d)
     try:
         # Create the path to the data directory corresponding to the current date
@@ -45,3 +47,49 @@ def writeDictToFile(d, filename):
         
 print(sensorMap)
 writeDictToFile(sensorMap, 'sensor-map.json')
+
+# Now that we have the sensor map, we can iterate through the data and check for presence of files
+analysis = pd.DataFrame()
+
+# Create a list of dates. To be used by the pandas DataFrame
+delta = endDate-startDate
+delta = delta.days
+dateList = [startDate + datetime.timedelta(days=x) for x in range(delta)]
+
+dateList = list(map(lambda x:x.strftime('%Y-%m-%d'), dateList))
+# Create a column with the list of dates
+analysis['date'] = dateList
+
+# Iterate through the sensor map
+for sensor in sensorMap:
+    print(sensor)
+    # directory = "data/%s"%sensor
+    availabilityList=[]
+    for date in dateList:
+        # Generate the directory path
+        path=os.path.join(sourcePathPrefix, d.strftime("%Y/%m/%d"), sourcePathSuffix)
+
+        # The file could be a csv or compressed csv (csv.gz)
+        filename1 = '%s_%s.csv'%(date, sensor)
+        filename1 = os.path.join(path, filename1)
+        filename2 = '%s_%s.csv.gz'%(date, sensor)
+        filename2 = os.path.join(path, filename2)
+        
+        try:
+            # Check if either file exists then insert a 1 to availability list
+            if os.path.isfile(filename1) or os.path.isfile(filename2):
+                availabilityList.append(str(1))
+            else:
+                # If either file doesn't exist, insert a 0
+                availabilityList.append(str(0))
+        except Exception as  e:
+            # If anything should go wrong while reading file, insert a 0
+            print(e)
+            availabilityList.append(str(0))
+
+    # Insert availability data into analysis df
+    analysis[sensor]=availabilityList
+
+# Save the dataframe as a csv
+analysis.to_csv('analysis.csv', index=False)
+# print(analysis)
