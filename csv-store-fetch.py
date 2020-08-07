b38f5065187aad3a8e92afddf721f90dc9ee39a2
Rohit Gupta <rohitrgupta@gmail.com>
2020-08-07 08:53:39 +0530

    feat:Updates csv copying script
    Does not anonymize sensor ids

--- csv-store-fetch.py
+++ csv-store-fetch.py
@@ -10,30 +10,37 @@ sourcePathPrefix=os.getenv('SOURCE_PREFIX', '/home/Data')
 sourcePathSuffix=os.getenv('SOURCE_SUFFIX', '/data')
 destPathPrefix=os.getenv('DEST_PREFIX', '/home/dest')
 
-starttime=datetime.datetime(2019,9,17,0,0)
-endtime=datetime.datetime(2020,5,12,0,0)
+startDateStr = os.getenv('START_DATE', '2019-01-31')
+endDateStr = os.getenv('END_DATE', '2019-12-31')
+
+startDate=datetime.datetime.strptime(startDateStr, '%Y-%m-%d')
+endDate=datetime.datetime.strptime(endDateStr, '%Y-%m-%d')
+
 sensorIdx=1
 sensorMap={}
-d=starttime
+d=startDate
 
-while d<= endtime:
+while d<= endDate:
     print(d)
     try:
         path=os.path.join(sourcePathPrefix, d.strftime("%Y/%m/%d"), sourcePathSuffix)
         for filename in sorted(os.listdir(path)):
+            print("Copying %s"%filename)
             sensorId = filename.split("_")[0]
-            if sensorId not in sensorMap:
-                sensorMap[sensorId] = 'R%s'%sensorIdx
-                sensorIdx+=1
-            idx = sensorMap[sensorId]
+            #if sensorId not in sensorMap:
+            #    sensorMap[sensorId] = 'R%s'%sensorIdx
+            #    sensorIdx+=1
+            #idx = sensorMap[sensorId]
             # print(sensorId, idx)
 
-            destDirectory = "data/%s"%(idx)
+            destDirectory = os.path.join(destPathPrefix,"%s"%(sensorId))
 
             if not os.path.exists(destDirectory):
                 os.mkdir(destDirectory)
-
-            shutil.copyfile(os.path.join(path,filename), os.path.join(destDirectory, filename.split("_")[1]))
+            try:
+                shutil.copyfile(os.path.join(path,filename), os.path.join(destDirectory, filename.split("_")[1]))
+            except Exception as e:
+                print(e)
     except Exception as e:
         print(e)
     # print(path)
